35ac808bb11efb81994c38873e784502574bcfe5
Qing Ma <qing.ma.web@gmail.com>
2018-04-03 22:04:49 +0200

    set database for testing and use DatabaseMigrations trait to migrate and rollback for every test

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -2,11 +2,13 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ThreadsTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /** @test */
     public function a_user_can_browse_threads()
     {
