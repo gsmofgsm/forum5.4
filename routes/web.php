c3afd6c76ba7c904b618ae0c9308950b370c87e7
Qing Ma <qing.ma.web@gmail.com>
2018-04-03 22:25:20 +0200

    add endpoint to make test pass

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/threads', 'ThreadsController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
