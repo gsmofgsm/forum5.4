7d55c04f363402f54a534af288343ce458b9aad2
Qing Ma <qing.ma.web@gmail.com>
2018-04-03 21:55:43 +0200

    first Feature test: a user can browse threads

---
+++ tests/Feature/ThreadsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ThreadsTest extends TestCase
+{
+    /** @test */
+    public function a_user_can_browse_threads()
+    {
+        $response = $this->get('/threads');
+
+        $response->assertStatus(200);
+    }
+}
