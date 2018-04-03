f96845fa5a11cdf8d12247285a934e45885f95bd
Qing Ma <qing.ma.web@gmail.com>
2018-04-03 22:27:11 +0200

    a user should see the threads' title on index page

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -12,8 +12,9 @@ class ThreadsTest extends TestCase
     /** @test */
     public function a_user_can_browse_threads()
     {
+        $thread = factory('App\Thread')->create();
         $response = $this->get('/threads');
 
-        $response->assertStatus(200);
+        $response->assertSee($thread->title);
     }
 }
