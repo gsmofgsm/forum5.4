749d0d4255183c233b53ed31f7ef9af87ca0f1a0
Qing Ma <qing.ma.web@gmail.com>
2018-04-03 23:04:12 +0200

    add and use reply-owner relation but add the test a level lower so that we could see more precise error message

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -18,7 +18,12 @@
                 @foreach($thread->replies as $reply)
                 <div class="card">
                     <div class="card-header">
+                        <a href="#">
+                        {{ $reply->owner->name }}
+                        </a>
+                        said
                         {{ $reply->created_at->diffForHumans() }}
+                        ...
                     </div>
                     <div class="card-body">
                         {{ $reply->body }}
