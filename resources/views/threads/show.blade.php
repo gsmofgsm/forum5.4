bcab1d3e0f4b6efd752bda80ed8b65a8bff4aa9a
Qing Ma <qing.ma.web@gmail.com>
2018-04-03 22:52:59 +0200

    a user can read replies of a thread

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -13,5 +13,19 @@
                 </div>
             </div>
         </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @foreach($thread->replies as $reply)
+                <div class="card">
+                    <div class="card-header">
+                        {{ $reply->created_at->diffForHumans() }}
+                    </div>
+                    <div class="card-body">
+                        {{ $reply->body }}
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
     </div>
 @endsection
---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ReadThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected $thread;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
+
+    /** @test */
+    public function a_user_can_browse_threads()
+    {
+        $response = $this->get('/threads');
+        $response->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_read_a_single_thread()
+    {
+        $response = $this->get('/threads/' . $this->thread->id);
+        $response->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        // Given we have a thread
+        // And that thread includes replies
+        $reply = factory('App\Reply')->create(['thread_id' => $this->thread->id]);
+
+        // When we visit a thread page
+        $response = $this->get('/threads/' . $this->thread->id);
+
+        // Then we should see the replies
+        $response->assertSee($reply->body);
+    }
+}
