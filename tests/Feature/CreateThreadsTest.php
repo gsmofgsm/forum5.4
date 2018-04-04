88edb205755b594cb89e69476ec5a6d45a8e583f
Qing Ma <qing.ma.web@gmail.com>
2018-04-04 19:46:20 +0200

    test and add create Thread

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function guests_may_not_create_threads()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+        $this->post('/threads', []);
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_create_new_forum_threads()
+    {
+        // Given we have a signed in user
+        $this->actingAs(factory('App\User')->create());
+
+        // when we hit the endpoint to create a new thread
+        $thread = factory('App\Thread')->make();
+        $this->post('/threads', $thread->toArray());
+
+        // Then, when we visit the thread page.
+        $this->get($thread->path())
+        // We should see the new thread
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+}
