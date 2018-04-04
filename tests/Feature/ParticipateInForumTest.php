a6124e14136137684e8dca9ea1e1c0c5d60d740c
Qing Ma <qing.ma.web@gmail.com>
2018-04-04 19:13:16 +0200

    unauthenticated_users_may_not_add_replies: because exception is expected, no real post data is necessary

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -12,6 +12,13 @@ class ParticipateInForumTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
+    public function unauthenticated_users_may_not_add_replies()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $this->post('/threads/1/replies', []);
+    }
+    /** @test */
     public function an_authenticated_user_may_participate_in_forum_threads()
     {
         // Given we have a authenticated user
