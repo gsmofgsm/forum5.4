58d15b334e0a5675e281a655fe54860eacc2148d
Qing Ma <qing.ma.web@gmail.com>
2018-04-04 19:25:28 +0200

    add reply form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -23,5 +23,18 @@
                 @endforeach
             </div>
         </div>
+        @if(auth()->check())
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                    {{ csrf_field() }}
+                    <textarea name="body" id="body" class="form-control" placeholder="Have something to say?"></textarea>
+                    <button type="submit" class="btn btn-default">Post</button>
+                </form>
+            </div>
+        </div>
+        @else
+            <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+        @endif
     </div>
 @endsection
