4bbf2ac77cd1828b95fb14cab8660e0ea61f257a
Qing Ma <qing.ma.web@gmail.com>
2018-04-04 17:23:43 +0200

    test an_authenticated_user_may_participate_in_forum_threads

---
+++ tests/Feature/ParticipateInForumTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ParticipateInForumTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function an_authenticated_user_may_participate_in_forum_threads()
+    {
+        // Given we have a authenticated user
+        $user = factory('App\User')->create();
+        $this->be($user);
+
+        // And an existing thread
+        $thread = factory('App\Thread')->create();
+
+        // When the user add a reply to the thread
+        $reply = factory('App\Reply')->create();
+        $this->post('/threads/'.$thread->id.'/replies', $reply->toArray());
+
+        // Then their reply should be visible on the page
+        $this->get($thread->path())
+            ->assertSee($reply->body);
+    }
+}
