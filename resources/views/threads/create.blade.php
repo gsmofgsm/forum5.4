3e569b7e019c27a85564993bd9d272893eeb77dc
Qing Ma <qing.ma.web@gmail.com>
2018-04-04 20:41:49 +0200

    Thread create form page

---
+++ resources/views/threads/create.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Create a New Thread</div>
+
+                    <div class="card-body">
+                        <form action="/threads" method="POST">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <label for="title">Title:</label>
+                                <input type="text" name="title" id="title" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="body">Body:</label>
+                                <textarea name="body" id="body" class="form-control" rows="8"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Publish</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
