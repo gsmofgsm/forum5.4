d0497788028f966bf213f8f4637e9821cfc25a5a
Qing Ma <qing.ma.web@gmail.com>
2018-04-05 20:37:09 +0200

    share channels with all pages in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \View::composer('threads.create', function ($view) {
+        \View::composer('*', function ($view) {
             $view->with('channels', \App\Channel::all());
         });
     }
