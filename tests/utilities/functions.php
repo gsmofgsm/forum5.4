0a035744b1bdb7ec6374f26b4c9717f5002f473f
Qing Ma <qing.ma.web@gmail.com>
2018-04-06 23:14:05 +0200

    test threads order by popularity

--- tests/utilities/functions.php
+++ tests/utilities/functions.php
@@ -1,9 +1,9 @@
 <?php
 
-function make($class, $attributes=[]) {
-    return factory($class)->make($attributes);
+function make($class, $attributes=[], $times = null) {
+    return factory($class, $times)->make($attributes);
 }
 
-function create($class, $attributes=[]) {
-    return factory($class)->create($attributes);
+function create($class, $attributes=[], $times = null) {
+    return factory($class, $times)->create($attributes);
 }
