a24d745a68888c847f23acb4c59a236f155851d7
Qing Ma <qing.ma.web@gmail.com>
2018-04-06 22:19:55 +0200

    Add metadata to thread show page

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
@@ -14,27 +14,32 @@
                         {{ $thread->body }}
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+
                 @foreach($thread->replies as $reply)
                     @include ( 'threads.reply' )
                 @endforeach
+
+                @if(auth()->check())
+                    <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                        {{ csrf_field() }}
+                        <textarea name="body" id="body" class="form-control" placeholder="Have something to say?"></textarea>
+                        <button type="submit" class="btn btn-default">Post</button>
+                    </form>
+                @else
+                    <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+                @endif
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <p>
+                            This thread was published {{ $thread->created_at->diffForHumans() }} by
+                            <a href="#">{{ $thread->creator->name }}</a>, and currently
+                            has {{ $thread->replies()->count() }} comments.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
-        @if(auth()->check())
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <form method="POST" action="{{ $thread->path() . '/replies' }}">
-                    {{ csrf_field() }}
-                    <textarea name="body" id="body" class="form-control" placeholder="Have something to say?"></textarea>
-                    <button type="submit" class="btn btn-default">Post</button>
-                </form>
-            </div>
-        </div>
-        @else
-            <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
-        @endif
     </div>
 @endsection
