5a078a866b00c24468bbbfd1f8be713eb11c69f2
Qing Ma <qing.ma.web@gmail.com>
2018-04-08 00:43:25 +0200

    use policy to authorise deleting a thread

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -110,9 +110,7 @@ class ThreadsController extends Controller
      */
     public function destroy($channel, Thread $thread)
     {
-        if($thread->user_id != auth()->id()){
-            abort(403, 'You do not have permission to do this.');
-        }
+        $this->authorize('update', $thread);
 
         $thread->delete();
         if(request()->wantsJson()) {
