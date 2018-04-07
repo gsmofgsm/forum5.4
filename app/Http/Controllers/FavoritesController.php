e22edefefb4e7ac4776d2c990a0778367c805ad8
Qing Ma <qing.ma.web@gmail.com>
2018-04-07 17:15:29 +0200

    UI for favoriting a reply

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -15,6 +15,7 @@ class FavoritesController extends Controller
 
     public function store(Reply $reply)
     {
-        return $reply->favorite();
+        $reply->favorite();
+        return back();
     }
 }
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,11 +1,21 @@
 <div class="card">
     <div class="card-header">
-        <a href="#">
-            {{ $reply->owner->name }}
-        </a>
-        said
-        {{ $reply->created_at->diffForHumans() }}
-        ...
+        <div class="level">
+            <h5 class="flex">
+                <a href="#">
+                    {{ $reply->owner->name }}
+                </a>
+                said
+                {{ $reply->created_at->diffForHumans() }}
+                ...
+            </h5>
+            <div>
+                <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+                    {{ csrf_field() }}
+                    <button type="submit" class="btn btn-default">Favorite</button>
+                </form>
+            </div>
+        </div>
     </div>
     <div class="card-body">
         {{ $reply->body }}
