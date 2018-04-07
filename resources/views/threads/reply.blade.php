666f8e2747cb4bf2404b50041726b11060c16d63
Qing Ma <qing.ma.web@gmail.com>
2018-04-07 17:22:58 +0200

    disable favorite button is already favorited

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -12,7 +12,7 @@
             <div>
                 <form method="POST" action="/replies/{{ $reply->id }}/favorites">
                     {{ csrf_field() }}
-                    <button type="submit" class="btn btn-default">
+                    <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
                         {{ $reply->favorites()->count() }}
                         {{ str_plural('Favorite', $reply->favorites()->count()) }}
                     </button>
