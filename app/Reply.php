5ecf1699349b265777799864956f2836602854df
Qing Ma <qing.ma.web@gmail.com>
2018-04-07 21:07:30 +0200

    Add a custom getter of favoritesCount because we deleted the old withCount

--- app/Reply.php
+++ app/Reply.php
@@ -31,4 +31,9 @@ class Reply extends Model
     {
         return !! $this->favorites->where('user_id', auth()->id())->count();
     }
+
+    public function getFavoritesCountAttribute()
+    {
+        return $this->favorites->count();
+    }
 }
