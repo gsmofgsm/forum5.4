22ae46a1cb3300717df8f7b64b7008e966e89cbf
Qing Ma <qing.ma.web@gmail.com>
2018-04-07 21:15:58 +0200

    extract trait Favoritable

--- app/Reply.php
+++ app/Reply.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
+    use Favoritable;
+
     protected $guarded = [];
     protected $with = ['owner', 'favorites'];
 
@@ -14,26 +16,4 @@ class Reply extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 
-    public function favorites()
-    {
-        return $this->morphMany(Favorite::class, 'favorited');
-    }
-
-    public function favorite()
-    {
-        $attributes = ['user_id' => auth()->id()];
-        if(! $this->favorites()->where($attributes)->exists()) {
-            $this->favorites()->create($attributes);
-        }
-    }
-
-    public function isFavorited()
-    {
-        return !! $this->favorites->where('user_id', auth()->id())->count();
-    }
-
-    public function getFavoritesCountAttribute()
-    {
-        return $this->favorites->count();
-    }
 }
