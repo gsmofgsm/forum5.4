9989a5a3b3971876241440fc22de9869e2c7ca60
Qing Ma <qing.ma.web@gmail.com>
2018-04-08 00:10:15 +0200

    guests_cannot_delete_threads

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -67,6 +67,15 @@ class CreateThreadsTest extends TestCase
     }
 
     /** @test */
+    public function guests_cannot_delete_threads()
+    {
+        $this->withExceptionHandling();
+        $thread = create('App\Thread');
+        $response = $this->delete($thread->path());
+        $response->assertRedirect('/login');
+    }
+
+    /** @test */
     public function a_thread_can_be_deleted()
     {
         $this->signIn();
