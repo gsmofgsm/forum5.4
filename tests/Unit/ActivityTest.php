cb812fade1db66960f055276161ef8d61642a9ce
Qing Ma <qing.ma.web@gmail.com>
2018-04-08 22:30:36 +0200

    it_records_activity_when_a_reply_is_created

--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -27,4 +27,12 @@ class ActivityTest extends TestCase
         $this->assertEquals($activity->subject->id, $thread->id);
     }
 
+    /** @test */
+    public function it_records_activity_when_a_reply_is_created()
+    {
+        $this->signIn();
+        $reply = create('App\Reply');
+
+        $this->assertEquals(2, Activity::count());
+    }
 }
