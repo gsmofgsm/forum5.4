08ecc3e5037887aabf2e237881b8de0143e3f2c1
Qing Ma <qing.ma.web@gmail.com>
2018-04-09 22:45:01 +0200

    higher order messaging - psydo property

--- app/Thread.php
+++ app/Thread.php
@@ -20,7 +20,7 @@ class Thread extends Model
         });
 
         static::deleting(function($thread){
-            $thread->replies->each(function($reply){ $reply->delete(); });
+            $thread->replies->each->delete();
         });
     }
 
