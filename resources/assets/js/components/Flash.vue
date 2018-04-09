218a50cf661b6ec1a6ec6526b66ceb3e1b1edca9
Qing Ma <qing.ma.web@gmail.com>
2018-04-09 23:21:49 +0200

    flash message with Vue

---
+++ resources/assets/js/components/Flash.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="alert alert-warning alert-flash" role="alert" v-show="show">
+        <strong>Success!</strong> {{ body }}
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['message'],
+
+        data() {
+            return {
+                body: '',
+                show: false
+            }
+        },
+
+        created() {
+            if (this.message) {
+                this.body = this.message;
+                this.show = true;
+
+                setTimeout(()=> {
+                    this.show = false;
+                }, 3000);
+            }
+        }
+    }
+</script>
+
+<style>
+    .alert-flash {
+        position: fixed;
+        right: 25px;
+        bottom: 105px;
+    }
+</style>
