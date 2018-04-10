a10b06bf554da50880a00bc3da10975c4907f61a
Qing Ma <qing.ma.web@gmail.com>
2018-04-10 22:38:07 +0200

    add delete reply button

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -23,4 +23,14 @@
     <div class="card-body">
         {{ $reply->body }}
     </div>
+
+    @can('update', $reply)
+    <div class="card-footer">
+        <form method="POST" action="/replies/{{ $reply->id }}">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button type="submit" class="btn btn-danger btn-xs">Delete</button>
+        </form>
+    </div>
+    @endcan
 </div>
