d93518c8af64315564db943ebf42b5d55bef3a5a
Qing Ma <qing.ma.web@gmail.com>
2018-04-11 00:19:14 +0200

    delete reply by ajax

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -39,6 +39,10 @@ class RepliesController extends Controller
 
         $reply->delete();
 
+        if(request()->expectsJson()){
+            return response(['status' => 'Reply deleted']);
+        }
+
         return back();
     }
 }
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -36,11 +36,12 @@
     @can('update', $reply)
     <div class="card-footer level">
         <button class="btn btn-xs mr-1" @click="editing = true">Edit</button>
-        <form method="POST" action="/replies/{{ $reply->id }}">
-            {{ csrf_field() }}
-            {{ method_field('DELETE') }}
-            <button type="submit" class="btn btn-danger btn-xs">Delete</button>
-        </form>
+        <button class="btn btn-xs btn-danger" @click="destroy">Delete</button>
+        {{--<form method="POST" action="/replies/{{ $reply->id }}">--}}
+            {{--{{ csrf_field() }}--}}
+            {{--{{ method_field('DELETE') }}--}}
+            {{--<button type="submit" class="btn btn-danger btn-xs">Delete</button>--}}
+        {{--</form>--}}
     </div>
     @endcan
 </div>
