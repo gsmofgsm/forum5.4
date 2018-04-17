26acf9a3c3f8d3d6b994063752b5c3b28c50b566
Qing Ma <qing.ma.web@gmail.com>
2018-04-18 00:07:40 +0200

    create a Spam Class for detecting spam with SpamTest

---
+++ app/Spam.php
@@ -0,0 +1,29 @@
+<?php
+namespace App;
+
+
+class Spam
+{
+    public function detect($body)
+    {
+        // Detect invalid keywords.
+
+        $this->detectInvalidKeywords($body);
+
+        return false;
+    }
+
+    protected function detectInvalidKeywords($body)
+    {
+        $invalidKeywords = [
+            'Yahoo customer support'
+        ];
+
+        foreach($invalidKeywords as $keyword){
+
+            if(stripos($body, $keyword) !== false){
+                throw new \Exception('Your reply contains spam.');
+            }
+        }
+    }
+}
