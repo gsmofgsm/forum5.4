d867bbb5ef961ad92b6ab0a830412773dcfcf65a
Qing Ma <qing.ma.web@gmail.com>
2018-04-23 22:52:14 +0200

    refactor where + if => whereIn

--- app/Listeners/NotifyMentionedUsers.php
+++ app/Listeners/NotifyMentionedUsers.php
@@ -26,18 +26,15 @@ class NotifyMentionedUsers
      */
     public function handle(ThreadReceivedNewReply $event)
     {
-
         // Inspect the body of the reply for username mentions
         preg_match_all('/\@([^\s\.]+)/', $event->reply->body, $matches);
 
         $names = $matches[1];
         // And then for each mentioned user, notify them.
-        foreach($names as $name){
-            $user = User::whereName($name)->first();
-
-            if($user) {
+        User::whereIn('name', $names)
+            ->get()
+            ->each(function($user) use ($event){
                 $user->notify(new YouWereMentioned($event->reply));
-            }
-        }
+            });
     }
 }
