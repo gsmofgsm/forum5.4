d48ab98d471d8a2081ca0fe11083e1d013925c7e
Qing Ma <qing.ma.web@gmail.com>
2018-04-24 23:03:26 +0200

    a little extra feature and refactor

---
+++ resources/views/threads/_list.blade.php
@@ -0,0 +1,23 @@
+@foreach($threads as $thread)
+    <article>
+        <div class="level">
+            <div class="flex">
+                <h4>
+                    <a href="{{ $thread->path() }}">
+                        @if(auth()->check() && $thread->hasUpdatesFor(auth()->user()))
+                            <strong>
+                                {{ $thread->title }}
+                            </strong>
+                        @else
+                            {{ $thread->title }}
+                        @endif
+                    </a>
+                </h4>
+                <h5>Posted: By: <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a></h5>
+            </div>
+            <a href="{{ $thread->path() }}"><strong>{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}</strong></a>
+        </div>
+        <div class="body">{{ $thread->body }}</div>
+    </article>
+    <hr>
+@endforeach
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -8,26 +8,7 @@
                     <div class="card-header">Forum Threads</div>
 
                     <div class="card-body">
-                        @foreach($threads as $thread)
-                            <article>
-                                <div class="level">
-                                <h4 class="flex">
-                                    <a href="{{ $thread->path() }}">
-                                        @if(auth()->check() && $thread->hasUpdatesFor(auth()->user()))
-                                            <strong>
-                                                {{ $thread->title }}
-                                            </strong>
-                                        @else
-                                            {{ $thread->title }}
-                                        @endif
-                                    </a>
-                                </h4>
-                                    <a href="{{ $thread->path() }}"><strong>{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}</strong></a>
-                                </div>
-                                <div class="body">{{ $thread->body }}</div>
-                            </article>
-                            <hr>
-                        @endforeach
+                        @include('threads._list')
                     </div>
                 </div>
             </div>
