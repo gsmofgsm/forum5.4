2a391b131544aa84136abab8da647cd587aec7b4
Qing Ma <qing.ma.web@gmail.com>
2018-05-18 22:07:21 +0200

    a little less try catch

--- app/Http/Controllers/Auth/RegisterConfirmationController.php
+++ app/Http/Controllers/Auth/RegisterConfirmationController.php
@@ -10,14 +10,13 @@ class RegisterConfirmationController extends Controller
 {
     public function index()
     {
-        try {
-            User::where('confirmation_token', request('token'))
-                ->firstOrFail()
-                ->confirm();
-        } catch (\Exception $e) {
-            return redirect('/threads')
-                ->with('flash', 'Unknown token.');
+        $user = User::where('confirmation_token', request('token'))->first();
+
+        if(! $user) {
+            return redirect('/threads')->with('flash', 'Unknown token.');
         }
+
+        $user->confirm();
         return redirect('/threads')
             ->with('flash', 'Your account is now confirmed! You may post to the forum.');
     }
