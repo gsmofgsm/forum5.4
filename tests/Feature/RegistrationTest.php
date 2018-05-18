2fb547b8a9c67afdae11e10da76136f45acc02c8
Qing Ma <qing.ma.web@gmail.com>
2018-05-18 22:19:42 +0200

    delete confirmation token after confirmed

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -44,6 +44,7 @@ class RegistrationTest extends TestCase
         $this->assertTrue($user->fresh()->confirmed);
 
         $response->assertRedirect('/threads');
+        $this->assertNull($user->fresh()->confirmation_token);
     }
 
     /** @test */
